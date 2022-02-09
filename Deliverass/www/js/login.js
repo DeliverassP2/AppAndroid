44426d73d69e6b8a1f289f0c1c61913ce3060b7f
AdrianSaut <adisautean@gmail.com>
2022-02-09 17:45:50 +0100

    Pantalles noves

--- Deliverass/www/js/login.js
+++ Deliverass/www/js/login.js
@@ -16,47 +16,75 @@
 //     });
 // });
 
+// $(document).ready(function() {
+//     $('#b1').click(function() {
+//         var user = $('#id_user').val();
+//         var pass = $('#password').val();
+//         $.post("http://192.168.1.203/login.php", { id: user, password: pass }, function(data) {
+//             if (data == true) {
+//                 window.location.href = "menu.html";
+//             } else {
+//                 alert("Usuario o contraseña incorrectos");
+//             }
+//         });
+//     });
+// });
 
 
-function loginCredencials() {
-    // var xhttp = new XMLHttpRequest();
-    var id = document.getElementById("id_user").value;
-    var password = document.getElementById("password").value;
-    console.log(id + password); 
-    enviarLogin(id, password);
-}
 
-function enviarLogin(id_usuari, pwd) {
-    $.post("http://192.168.1.203/login.php", {id_usuari: id_usuari, pwd: pwd}, 
-    function(response) {
-        if (response.result == true) {
-            window.location.href("menu.html");
-        }else if (response.result == false) {
-            alert("Error");
-        }
-    }, 'json');
-}
+// function loginCredencials() {
+//     // var xhttp = new XMLHttpRequest();
+//     var id = document.getElementById("id_user").value;
+//     var password = document.getElementById("password").value;
+//     console.log(id + password); 
+//     enviarLogin(id, password);
+// }
 
+// function enviarLogin(id_usuari, pwd) {
+//     $.post("http://192.168.1.203/login.php", {id_usuari: id_usuari, pwd: pwd}, 
+//     function(response) {
+//         if (response.result == true) {
+//             window.location.href("menu.html");
+//         }else if (response.result == false) {
+//             alert("Error");
+//         }
+//     }, 'json');
+// }
+
+
+// $.('#form').submit(function() {
+//         var id_user = $('#id_user').val();
+//         var password = $('#password').val();
+
+//         $.postJSON('http://192.168.1.203/login.php', { id: id_user, pwd: password }).done(function(data) {
+//             if (data.result == true) {
+//                 window.location.href("menu.html");
+//             } else if (data.result == false) {
+//                 alert("Error");
+//             }
+//         })
+//         return false;
+//     })
 // $(document).ready(function () {
-    // var url = "http://192.168.1.203/login.php";
-    // $("#b1").click(function () {
-    //     var id = $.trim($("#id_user").val());
-    //     var pw = $.trim($("#password").val());
-    //     var loginString = "id=" + id + "&password=" + pw;
-    //     $.ajax({
-    //         type: "POST", crossDomain: true, cache: false,
-    //         url: url,
-    //         data: loginString,
-    //         success: function (data) {
-    //             if (data == "success") {
-    //                 localStorage.loginStatus = "true";
-    //                 console.log("Envia dades");
-    //                 window.location.href = "menu.html";
-    //             } else if(data == "error") {
-    //                 alert("Error, les credencials no son correctes.");
-    //                 console.log(loginString);
-    //             }
-    //         }
-    //     });
-    // });
+// var url = "http://192.168.1.203/login.php";
+// $("#b1").click(function () {
+//     var id = $.trim($("#id_user").val());
+//     var pw = $.trim($("#password").val());
+//     var loginString = "id=" + id + "&password=" + pw;
+//     $.ajax({
+//         type: "POST", crossDomain: true, cache: false,
+//         url: url,
+//         data: loginString,
+//         success: function (data) {
+//             if (data == "success") {
+//                 localStorage.loginStatus = "true";
+//                 console.log("Envia dades");
+//                 window.location.href = "menu.html";
+//             } else if(data == "error") {
+//                 alert("Error, les credencials no son correctes.");
+//                 console.log(loginString);
+//             }
+//         }
+//     });
+// });
 // });
--- PHP de l'app/login.php
+++ PHP de l'app/login.php
@@ -2,56 +2,19 @@
 
 $db_connection = pg_connect("host=192.168.1.203 port=5432 dbname=Deliverass user=postgres password=Fat/3232");
 
-// $id = $_POST['id'];
-// $password = $_POST['password'];
-
-// if ($id !="" && $password !=""){
-//     $result = pg_query($db_connection, "SELECT * FROM DEL_usuaris WHERE id_usuari = '".$id."' AND password = '".$password."'");
-//     $row = pg_fetch_array($result);
-//     if ($row['id_usuari'] == $id && $row['password'] == $password){
-//         echo json_encode(array('result' => true));
-//     }else{
-//         echo json_encode(array('result' => false));
-//     }
-    
-// }
-
 $id = $_POST['id'];
 $password = $_POST['password'];
-    if(isset($_POST['register'])){   
-        $register = mysqli_num_rows(mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email'"));
-        if($register == 0)
-        {
-            $insert = mysqli_query($con,"INSERT INTO `users` (`email`,`password`) VALUES ('$email','$password')");
-            if($insert)
-                echo "success";
-            else
-                echo "error";
-        }
-        else if($register != 0)
-            echo "exist";
-    }
-    else if(isset($_POST['login'])){
+$resultats = array();
+    if(isset($_POST['login'])){
         $login = pg_query($db_connection, "SELECT * FROM DEL_usuaris WHERE id_usuari = '".$id."' AND password = '".$password."'");
-        
-        // $login = mysqli_num_rows(mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'"));
-        if($login != 0)
-            echo "success";
-        else
+        $row = pg_fetch_array($login);
+        if($row != 0){
+            $resultats["reult"] = true;
+        }else{
+            $resultats["reult"] = false;
+        }
             echo "error";
     }
-
-// echo $id . " " . $password;
-// if (isset($_POST['login'])) {
-
-//     if($login != 0){
-//         echo "success";
-//     }else{
-//         echo "success";
-//     }
-// }
-
-
-
 pg_close($db_connection);
+
 ?>
